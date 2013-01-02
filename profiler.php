053658030646ee861fbb79473cc1e38f9ebd31f6
Gordon Murray <projects@murrion.com>
2013-01-02 23:04:21 +0000

    Updated the output to put 1 mysql query per line

--- profiler.php
+++ profiler.php
@@ -7,7 +7,7 @@
 $mysql_log_name = 'Localhost.log'; // name of your log file
 $mysql_log_path = 'C:\\xampp\\mysql\\data\\'; // location of your log file
 $mysql_log = file($mysql_log_path . $mysql_log_name);
-$include_statements = array('select', 'from', 'where', 'join', 'order', 'sort'); //mysql keywords to look out for
+$include_statements = array('select', 'from', 'where', 'join', 'order', 'sort', 'and'); //mysql keywords to look out for
 
 /**
  * Check to see if one of the sql keywords is in the log item
@@ -37,31 +37,54 @@ function is_mysql($log_item, $include_statements)
     }
     else
     {
+        $query = '';
+        $query_started = false;
+        $query_updated = false;
+        $time = '';
         foreach ($mysql_log as $log)
         {
-            $time='';
-            $query ='';
-            
             $log_item_array = explode("\t", trim($log), 3);
-            
+
             if (isset($log_item_array[0]) && substr($log_item_array[0], 0, 6) == date("ymd"))
+            {
                 $time = str_replace(date("ymd"), "", $log_item_array[0]);
-            
+            }
+
             if ((isset($log_item_array[0]) && is_mysql($log_item_array[0], $include_statements) == true) || (isset($log_item_array[1]) && is_mysql($log_item_array[1], $include_statements) == true))
+            {
+                $clean_log_item = strtolower(trim(preg_replace('/\s+/', ' ', $log_item_array[0])));
+
+                if (is_numeric(substr($clean_log_item, 0, 1)) && substr($clean_log_item, 2, 5) == 'query')
+                {
+                    /*
+                     * start of a new query
+                     */
+                    $query_started = true;
+                    $query = " " . preg_replace('/\s+/', ' ', trim($log_item_array[1]));
+                }
+                else
+                {
+                    $query_started = false;
+                }
+            }
+
+            if (is_mysql($log_item_array[0], $include_statements) == true)
+            {
+                $query .= " " . $log_item_array[0];
+                $query_updated = true;
+            }
+            else
+            {
+                $query_updated = false;
+            }
+
+            if ($query != '' && $query_started == false && $query_updated == false)
             {
                 echo "<tr>\n";
                 echo "\t<td>$time</td>\n";
-                echo "\t<td>";
-                if (isset($log_item_array[0]) && $log_item_array[0] != '')
-                {
-                    echo (substr($log_item_array[0], 0, 6) != date("ymd")) ? trim(preg_replace('/\s+/', ' ', $log_item_array[0])) : "";
-                }
-                if (isset($log_item_array[1]) && $log_item_array[1] != '')
-                {
-                    echo " --- " . preg_replace('/\s+/', ' ', trim($log_item_array[1]));
-                }
-                echo "</td>\n";
+                echo "\t<td>$query</td>\n";
                 echo "</tr>\n";
+                $query = '';
             }
         }
     }
