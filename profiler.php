602276f1f06fed3dac824216c3b071a127979f45
Gordon Murray <projects@murrion.com>
2013-01-02 22:33:54 +0000

    Only show mysql related commands from the log file

--- profiler.php
+++ profiler.php
@@ -7,9 +7,29 @@
 $mysql_log_name = 'Localhost.log'; // name of your log file
 $mysql_log_path = 'C:\\xampp\\mysql\\data\\'; // location of your log file
 $mysql_log = file($mysql_log_path . $mysql_log_name);
+$include_statements = array('select', 'from', 'where', 'join', 'order', 'sort'); //mysql keywords to look out for
+
+/**
+ * Check to see if one of the sql keywords is in the log item
+ * @param string $log_item
+ * @param array $include_statements
+ * @return boolean $mysql
+ */
+function is_mysql($log_item, $include_statements)
+{
+    $mysql = false;
+    foreach ($include_statements as $phrase)
+    {
+        if (stristr($log_item, $phrase))
+        {
+            $mysql = true;
+        }
+    }
+    return $mysql;
+}
 ?>
 
-<table width="100%">
+<table width="100%" border="1">
     <?php
     if (empty($mysql_log))
     {
@@ -20,15 +40,24 @@ $mysql_log = file($mysql_log_path . $mysql_log_name);
         foreach ($mysql_log as $log)
         {
             $log_item_array = explode("\t", trim($log), 3);
-            echo "<tr>\n";
-            echo "\t<td>";
-            echo (substr($log_item_array[0], 0, 6) == date("ymd")) ? str_replace(date("ymd"), "", $log_item_array[0]) : "";
-            echo "</td>\n";
-            echo "\t<td>";
-            echo (substr($log_item_array[0], 0, 6) != date("ymd")) ? trim(preg_replace('/\s+/', ' ', $log_item_array[0])) : "";
-            echo (isset($log_item_array[1])) ? " " . preg_replace('/\s+/', ' ', trim($log_item_array[1])) : "";
-            echo "</td>\n";
-            echo "</tr>\n";
+            if (isset($log_item_array[0]) && substr($log_item_array[0], 0, 6) == date("ymd"))
+                $time = str_replace(date("ymd"), "", $log_item_array[0]);
+            if ((isset($log_item_array[0]) && is_mysql($log_item_array[0], $include_statements) == true) || (isset($log_item_array[1]) && is_mysql($log_item_array[1], $include_statements) == true))
+            {
+                echo "<tr>\n";
+                echo "\t<td>$time</td>\n";
+                echo "\t<td>";
+                if (isset($log_item_array[0]) && $log_item_array[0] != '')
+                {
+                    echo (substr($log_item_array[0], 0, 6) != date("ymd")) ? trim(preg_replace('/\s+/', ' ', $log_item_array[0])) : "";
+                }
+                if (isset($log_item_array[1]) && $log_item_array[1] != '')
+                {
+                    echo " " . preg_replace('/\s+/', ' ', trim($log_item_array[1]));
+                }
+                echo "</td>\n";
+                echo "</tr>\n";
+            }
         }
     }
     ?>
